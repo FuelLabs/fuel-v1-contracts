1bc1151aa2daefbad3c0f7e526429b9e8edbb25f
SilentCicero <thenickdodson@gmail.com>
2020-07-03 14:34:56 -0400

    benchmark package adjustments

--- src/benchmarks/burning.js
+++ src/benchmarks/burning.js
@@ -1,7 +1,7 @@
 // 75,000 one-off points burning
 
-const { test, utils, overrides } = require('fuel-common/environment');
-const { chunk, pack, combine } = require('fuel-common/struct');
+const { test, utils, overrides } = require('@fuel-js/common/environment');
+const { chunk, pack, combine } = require('@fuel-js/common/struct');
 const { bytecode, abi, errors } = require('../builds/Fuel.json');
 const Proxy = require('../builds/Proxy.json');
 const ERC20 = require('../builds/ERC20.json');
