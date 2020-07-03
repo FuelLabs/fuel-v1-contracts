7ca231ab04a1d4f5026ade8c1ac6b071474855af
SilentCicero <thenickdodson@gmail.com>
2020-07-03 14:40:15 -0400

    v1.0.0 working benchmarks with new imports

--- src/benchmarks/minting.js
+++ src/benchmarks/minting.js
@@ -6,9 +6,9 @@ const { bytecode, abi, errors } = require('../builds/Fuel.json');
 const Proxy = require('../builds/Proxy.json');
 const ERC20 = require('../builds/ERC20.json');
 const { BlockHeader, RootHeader, Leaf,
-    merkleTreeRoot, transactions, hashes } = require('@fuel-js/protocol/block');
-const tx = require('@fuel-js/protocol/transaction');
-const { Deposit } = require('@fuel-js/protocol/deposit');
+    merkleTreeRoot, transactions, hashes } = require('@fuel-js/protocol/src/block');
+const tx = require('@fuel-js/protocol/src/transaction');
+const { Deposit } = require('@fuel-js/protocol/src/deposit');
 const { defaults } = require('../tests/harness');
 
 module.exports = test('100k Points Claims', async t => { try {
