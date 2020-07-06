c5fe376b06dfa748c3c0fa8e722ac934ba90c00e
John Adler <john.adler@protonmail.ch>
2020-07-05 21:33:25 -0400

    Rename transaction hash id to transaction id.

--- src/examples/IFuel.sol
+++ src/examples/IFuel.sol
@@ -2,7 +2,7 @@ contract IFuel {
   function deposit(address account, address token) external;
   function commitRoot(bytes32 merkleTreeRoot, uint256 token,  uint256 fee, bytes transactions) external;
   function commitBlock(uint32 minimum, uint32 height, bytes32[] roots) external;
-  function commitWitness(bytes32 transactionHashId) external;
+  function commitWitness(bytes32 transactionId) external;
   function commitAddress(address addr) external returns (uint256 id);
   function withdraw(bytes proof) external;
   function bondWithdraw(bytes blockHeader) external;
@@ -18,13 +18,13 @@ contract IFuel {
   function verifyTransaction(bytes proof, uint8 finalization) external view returns (bool);
   function verifyMetadata(bytes8 metadata, bytes proof) external view returns (bool);
 
-  function selectUTXO(bytes proof) external view returns (bytes32 transactionHashId, uint8 outputIndex, uint8 outputType, address owner, uint256 amount, uint32 token, bytes32 digest, uint256 expiry, address returnOwner);
+  function selectUTXO(bytes proof) external view returns (bytes32 transactionId, uint8 outputIndex, uint8 outputType, address owner, uint256 amount, uint32 token, bytes32 digest, uint256 expiry, address returnOwner);
   function selectOutput(bytes proof) external view returns (bytes output);
   function selectMetadata(bytes proof, uint8 index) external view returns (bytes8 metadata);
   function outputMetadata(bytes proof) external view returns (uint256 id);
   function inputMetadata(bytes proof) external view returns (uint256 id);
 
-  function witnessAt(address account, address blockNumber) external view returns (bytes32 transactionHashId);
+  function witnessAt(address account, address blockNumber) external view returns (bytes32 transactionId);
   function funnel(address account) external view returns (address);
   function blockProducer() external view returns (address blockProducer);
   function blockTip() external view  returns (uint256 blockTip);
@@ -53,5 +53,5 @@ contract IFuel {
   event AddressIndexed(address indexed owner, uint256 indexed id);
   event TokenIndexed(address indexed token, uint256 indexed id);
   event DepositMade(address indexed account, address indexed token, uint256 amount);
-  event WithdrawalMade(address indexed account,address token,uint256 amount,uint256 indexed blockHeight,uint256 rootIndex,bytes32 indexed transactionLeafHash,uint8 outputIndex,bytes32 transactionHashId);
+  event WithdrawalMade(address indexed account,address token,uint256 amount,uint256 indexed blockHeight,uint256 rootIndex,bytes32 indexed transactionLeafHash,uint8 outputIndex,bytes32 transactionId);
 }
