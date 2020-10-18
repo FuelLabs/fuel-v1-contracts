d05f3afd0ee9412436dbbab7a8c51f57d855e435
SilentCicero <thenickdodson@gmail.com>
2020-10-18 12:03:09 -0400

    positive simulation

--- src/tests/simulation.js
+++ src/tests/simulation.js
@@ -1,14 +1,19 @@
-// here we will do an optimistic test of functionality
-// build Fuel, with proxy
-// build a few blocks
-// withdraw some funds
-// htlc out of chain
-// bond withdraw out of chain
-// build more blocks
-// present fraud, tackle fraud
-// build more blocks
-// present foreign fraud, tackle fraud
-// build more blocks
+/*
+Purpose:
+This test will simulate mainnet Fuel usage and setup.
+
+1) deployment
+2) valid blocks
+3) withraw funds
+4) htlc out of chain
+5) commit fraud, produce a few invalid blocks, revert chain
+6) produce valid blocks
+7) do a swap
+8) withdraw block bond
+9) produce blocks
+10) gather fees from roots
+11) withdraw fees
+*/
 const { test, utils, overrides } = require('@fuel-js/environment');
 const { chunk, pack, combine, chunkJoin } = require('@fuel-js/struct');
 const { bytecode, abi, errors } = require('../builds/Fuel.json');
@@ -86,6 +91,94 @@ module.exports = test('simualtion', async t => {
         });
         const userADepositTokenTx = await t.wait(contract.deposit(userA, erc20.address, overrides),
             'ether deposit', errors);
+
+        // Set fee stipulations.
+        let feeToken = 1;
+        let fee = utils.parseEther('.000012');
+
+        // Make a block.
+        async function makeBlock(opts = {}) {
+            // Fake leafs.
+            const fakeLeaf = new Leaf({
+                data: chunk(utils.hexlify(utils.randomBytes(120))),
+            });
+
+            // Produce Transactions.
+            const txs = [fakeLeaf];
+
+            // Produce Root.
+            const root = (new RootHeader({
+                rootProducer: producer,
+                merkleTreeRoot: merkleTreeRoot(txs),
+                commitmentHash: utils.keccak256(combine(txs)),
+                rootLength: utils.hexDataLength(combine(txs)),
+                feeToken: feeToken,
+                fee: fee,
+            }));
+
+            // Root is committed from a different address than the Proxy.
+            await t.wait(contract.commitRoot(
+                root.properties.merkleTreeRoot().get(),
+                0,
+                0,
+                combine(txs),
+                overrides,
+            ), 'valid submit', errors);
+
+            // Get block tip.
+            const blockTip = (await contract.blockTip()).add(1);
+
+            // Produce block.
+            const header = (new BlockHeader({
+                producer: proxy.address,
+                height: blockTip,
+                numTokens: 1,
+                numAddresses: 1,
+                roots: [root.keccak256Packed()],
+            }));
+
+            // Commit block on-chain.
+            const currentBlock = await t.provider.getBlockNumber();
+            const currentBlockHash = (await t.provider.getBlock(currentBlock)).hash;
+
+            // Commit transaction encoded data.
+            const commitTx = contract.interface.functions.commitBlock.encode([
+                currentBlock,
+                currentBlockHash,
+                blockTip,
+                [root.keccak256Packed()],
+            ]);
+
+            // Produce block commitment from the Proxy contract.
+            await t.wait(t.wallets[0].sendTransaction({
+                ...overrides,
+                value: await contract.BOND_SIZE(),
+                to: proxy.address,
+            }), 'ether to proxy');
+            const block = await t.wait(
+                proxy.transact(
+                    contract.address,
+                    await contract.BOND_SIZE(),
+                    commitTx,
+                    overrides,
+                ),
+                'commit block',
+                errors);
+
+            header.properties.blockNumber().set(block.logs[0].blockNumber);
+            t.equalBig(await contract.blockTip(), 1, 'tip');
+
+            // Return the headers and data.
+            return {
+                block: header,
+                root,
+                txs,
+            };
+        }
+
+        // Make two blocks.
+        await makeBlock();
+        await makeBlock();
     }
   
     // Produce State.
